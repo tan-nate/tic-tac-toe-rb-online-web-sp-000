54f4e9bd0c4f2426fe7b802a4489dab84a42350b
Nate Tan <nktan93@gmail.com>
2019-03-20 16:21:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -29,3 +29,7 @@ def position_taken?(board, location)
   board[location] != " " && board[location] != ""
 end
 
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
