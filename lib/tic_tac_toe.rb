169394524a318588d79372e361fe84acf817f3e2
Nate Tan <nktan93@gmail.com>
2019-03-20 16:29:46 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -63,3 +63,46 @@ def current_player(board)
   end
 end
 
+def won?(board)
+  game_win_combinations = WIN_COMBINATIONS.select do |win_combination|
+    win_combination.all? {|win_index| board[win_index] == "X"} ||
+    win_combination.all? {|win_index| board[win_index] == "O"}
+  end
+  if game_win_combinations == []
+    game_win_combinations = false
+  else
+    game_win_combinations.flatten!
+  end
+  game_win_combinations
+end
+
+def full?(board)
+  (0..8).to_a.all? do |index|
+    position_taken?(board, index)
+  end
+end
+
+def draw?(board)
+  if !won?(board) && full?(board)
+    true
+  else
+    false
+  end
+end
+
+def over?(board)
+  if won?(board) || draw?(board) || full?(board)
+    true
+  else
+    false
+  end
+end
+
+def winner(board)
+  if won?(board)
+    board[won?(board)[0]]
+  else
+    nil
+  end
+end
+
